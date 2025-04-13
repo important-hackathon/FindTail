359870722aced0822bf887ef23a8bca1f00c4ebd
Ostap Sidoryak <ostapsidorak@gmail.com>
2025-04-13 04:30:38 +0300

    styles/shelter/[id] page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // This allows images from any host
+        pathname: "/**", // This allows images from any path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/shelters/ShelterInfo.tsx
+++ src/components/shelters/ShelterInfo.tsx
@@ -13,24 +13,31 @@ const ShelterInfo = ({ shelter }: ShelterInfoProps) => {
     ) || shelter.shelter_details.shelter_type;
 
   return (
-    <div>
-      <h2 className="text-lg font-semibold mb-2">Інформація про притулок</h2>
-      <div className="space-y-2 text-gray-600">
+    <div className="text-[#432907] pb-10">
+      <h2 className="text-lg font-semibold mb-2 ">Інформація про притулок</h2>
+      <div className="space-y-2 ">
         <p>
-          <span className="font-medium">Тип:</span>{" "}
-          {shelterTypeLabel?.label || shelterTypeLabel}
-        </p>
-        <p>
-          <span className="font-medium">Розташування:</span>{" "}
+          <span className="font-semibold">Адреса:</span>{" "}
           {shelter.shelter_details.location}
         </p>
         <p>
-          <span className="font-medium">Контакти:</span>{" "}
-          {shelter.phone_number || "Не вказано"}
+          <span className="font-semibold">Тип:</span>{" "}
+          {shelterTypeLabel?.label || shelterTypeLabel}
         </p>
+        <p>
+          <span className="font-semibold">Рейтинг:</span>{" "}
+          {shelter.shelter_details.rating}
+        </p>
+        <p>
+          <span className="font-semibold">Приймає тварин:</span> Так
+        </p>
+        <p>
+          <span className="font-semibold">Донати приймаються:</span> Так
+        </p>
+
         {shelter.shelter_details.website && (
           <p>
-            <span className="font-medium">Вебсайт:</span>{" "}
+            <span className="font-semibold">Вебсайт:</span>{" "}
             <a
               href={shelter.shelter_details.website}
               target="_blank"
